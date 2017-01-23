41fec28567f5e763d1605805142c388217c38779
chris <B00291253@studentmail.uws.ac.uk>
2017-01-23 16:55:13 +0000

    Adding Line Line Collision

--- lab2/CollisionDetection.cpp
+++ lab2/CollisionDetection.cpp
@@ -1,7 +1,6 @@
 #include "CollisionDetection.h"
 
 
-
 CollisionDetection::CollisionDetection()
 {
 	this->collisionTest = new CollisionTest2D();
@@ -11,4 +10,15 @@ CollisionDetection::CollisionDetection()
 CollisionDetection::~CollisionDetection()
 {
 	delete(collisionTest);
+}
+
+vec2* CollisionDetection::LineLine2DCollision(ImplicitLine& lineA, ImplicitLine& lineB) {
+	if (collisionTest->LineLine2DCollisionTest(lineA, lineB)) {
+		mat2 A(lineA.a, lineA.b, lineB.a, lineB.b);
+		vec2 b(-lineA.c, -lineB.c);
+		mat2 iA = inverse(A);
+		vec2 x = iA*b;
+		return &x;
+	}
+	return nullptr;
 }
--- lab2/CollisionDetection.h
+++ lab2/CollisionDetection.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "CollisionTest.h"
+
 class CollisionDetection
 {
 public:
 	CollisionDetection();
 	~CollisionDetection();
 
-	vec2 LineLine2DCollision(ImplicitLine& lineA, ImplicitLine& lineB);
-	vec2 LineCircle2DCollision(ImplicitLine& line, Circle& circle);
-	vec2 CircleCircle2DCollision(Circle& circleA, Circle& circleB);
+	vec2* LineLine2DCollision(ImplicitLine& lineA, ImplicitLine& lineB);
+	vec2* LineCircle2DCollision(ImplicitLine& line, Circle& circle);
+	vec2* CircleCircle2DCollision(Circle& circleA, Circle& circleB);
 private:
 	CollisionTest2D* collisionTest;
 };
